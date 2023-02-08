e3e2eabc8718e32e0b9afde24ae78e17cc6c4611
Zain Asif <zainclasher2002@gmail.com>
2023-02-08 22:45:22 +0500

    Feat:  Update

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,7 +1,21 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
-  alias Discuss.Topic
+  alias Discuss.{Topic, Repo}
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset)
+    end
+  end
 
   def edit(conn, %{"id" => topic_id}) do
     topic = Repo.get(Topic, topic_id)
@@ -25,10 +39,10 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, _post} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
-        |> redirect(to: topic_path(conn, :index))
+        |> redirect(to: Routes.topic_path(conn, :index))
 
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
