26ebfbe9ddea5299a96ad3d223cae735b49b7118
Zain Asif <zainclasher2002@gmail.com>
2023-02-15 00:05:00 +0500

    fix

--- config/config.exs
+++ config/config.exs
@@ -53,7 +53,7 @@ import_config "#{config_env()}.exs"
 
 config :ueberauth, Ueberauth,
   providers: [
-    github: {Ueberauth.Strategy.Github, []}
+    github: {Ueberauth.Strategy.Github, [default_scope: "user:email"]}
   ]
 
 config :ueberauth, Ueberauth.Strategy.Github.OAuth,
