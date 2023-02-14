3644d020d7a198b676257967d3c6c559c716d9f6
Zain Asif <zainclasher2002@gmail.com>
2023-02-14 15:35:39 +0500

    configured Ueberauth

--- config/config.exs
+++ config/config.exs
@@ -50,3 +50,12 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: {Ueberauth.Strategy.Github, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "4944b121992bf62f9dee",
+  client_secret: "c5eef119a45ebe706ff0d5b0d5752f8890c3f059"
