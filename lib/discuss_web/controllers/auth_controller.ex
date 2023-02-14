85c8d34233339b7efcd17b81967b532d131791ea
Zain Asif <zainclasher2002@gmail.com>
2023-02-14 23:46:02 +0500

    func: Signin

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -4,11 +4,26 @@ defmodule DiscussWeb.AuthController do
 
   alias Discuss.{User, Repo}
 
-  def callback(%{assigns: %{ueberauth: auth}} = conn, params) do
+  def callback(%{assigns: %{ueberauth: auth}} = conn, _params) do
     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
     changeset = User.changeset(%User{}, user_params)
 
-    insert_or_update_user(changeset)
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error Signing in")
+        |> redirect(to: Routes.topic_path(conn, :index))
+    end
   end
 
   defp insert_or_update_user(changeset) do
