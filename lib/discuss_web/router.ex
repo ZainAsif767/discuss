b26d318eddcd6a31ddcb33988629149493989352
Zain Asif <zainclasher2002@gmail.com>
2023-02-16 08:40:33 +0500

    plugs for Setuser

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -8,6 +8,7 @@ defmodule DiscussWeb.Router do
     plug :put_root_layout, {DiscussWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
