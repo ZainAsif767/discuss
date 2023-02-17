b61dec3c8cc5b14775c00c88b4e387c5d1dd1703
Zain Asif <zainclasher2002@gmail.com>
2023-02-17 16:49:29 +0500

    feat: Signout

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -30,6 +30,7 @@ defmodule DiscussWeb.Router do
   scope "/auth", DiscussWeb do
     pipe_through :browser
 
+    get "/signout", AuthController, :signout
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
   end
