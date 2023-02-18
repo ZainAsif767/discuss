77898cdde8440c05e92f270f9069c2decbcfc6b5
Zain Asif <zainclasher2002@gmail.com>
2023-02-19 01:28:49 +0500

    user sockets

---
+++ lib/discuss_web/channels/user_socket.ex
@@ -0,0 +1,19 @@
+defmodule DiscussWeb.UserSocket do
+  use Phoenix.Socket
+
+  channel "comments:*", DiscussWeb.CommentsChannel
+
+  # transport(:websocket, Phoenix.Transports.WebSocket)
+
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "key", token) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+
+      {:error, _error} ->
+        :error
+    end
+  end
+
+  def id(_socket), do: nil
+end
