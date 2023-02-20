cfed700e744f90cb1cd5a9d0ed5bca1bc72c8d5f
Zain Asif <zainclasher2002@gmail.com>
2023-02-20 07:35:40 +0500

    Feat: Preload & Broadcast

--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -5,21 +5,27 @@ defmodule DiscussWeb.CommentsChannel do
 
   def join("comments:" <> topic_id, _params, socket) do
     topic_id = String.to_integer(topic_id)
-    topic = Repo.get(Topic, topic_id)
+
+    topic =
+      Topic
+      |> Repo.get(topic_id)
+      |> Repo.preload(comments: [:user])
 
     {:ok, %{}, assign(socket, :topic, topic)}
   end
 
   def handle_in(_name, %{"content" => content}, socket) do
     topic = socket.assigns.topic
+    user_id = socket.assigns.user_id
 
     changeset =
       topic
-      |> build_assoc(:comments)
+      |> build_assoc(:comments, user_id: user_id)
       |> Comment.changeset(%{content: content})
 
     case Repo.insert(changeset) do
-      {:ok, _comment} ->
+      {:ok, comment} ->
+        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new", %{comment: comment})
         {:reply, :ok, socket}
 
       {:error, _reason} ->
